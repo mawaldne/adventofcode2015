e7bd685a0c316778470497afe30fd315a4f22b0c
Mike Waldner <mike.waldner@redfall.com>
2015-12-30 20:31:53 -0700

    Day 4.

---
+++ day4/five.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby -w
+
+require 'digest/md5'
+
+secret = ARGV[0]
+
+i = 1
+loop do
+  digest = Digest::MD5.hexdigest("#{secret}#{i}")
+  if digest[0..4] == '00000'
+      puts digest 
+      puts i
+      break 
+  end
+  i += 1
+end
+
