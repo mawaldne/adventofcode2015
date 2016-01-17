a232a275ce50f2b6846ab6e61536485937b9be76
Mike Waldner <mike.waldner@redfall.com>
2016-01-17 16:13:03 -0700

    Formatting

--- day6/lights.rb
+++ day6/lights.rb
@@ -2,46 +2,46 @@
 
 lights = Hash.new(0)
 
-def parse_start_end(instruction)
-  start, endz = instruction.gsub(/turn on |toggle |turn off | through/, '').split(" ")
-  x_s, y_s = start.split(",").map { |i| i.to_i } 
-  x_e, y_e = endz.split(",").map { |i| i.to_i } 
-  return [[x_s, y_s],[x_e, y_e]] 
-end 
+def parse_first_end(instruction)
+  first, endz = instruction.gsub(/turn on |toggle |turn off | through/, '').split(" ")
+  x_s, y_s = first.split(",").map { |i| i.to_i }
+  x_e, y_e = endz.split(",").map { |i| i.to_i }
+  return [[x_s, y_s],[x_e, y_e]]
+end
 
-def iterate(start, endz)
-  x_s, y_s = start
+def iterate(first, endz)
+  x_s, y_s = first
   x_e, y_e = endz
 
-  for x in x_s..x_e 
+  for x in x_s..x_e
     for y in y_s..y_e
       yield(x, y)
-    end 
-  end 
-end 
+    end
+  end
+end
 
 def turn_on(x, y, lights)
-  lights[[x,y]] += 1 
-end 
+  lights[[x,y]] += 1
+end
 
 def turn_off(x, y, lights)
   lights[[x,y]] -= 1 if lights[[x,y]] > 0
-end 
+end
 
 def toggle(x, y, lights)
   lights[[x,y]] += 2
-end 
+end
 
 ARGF.each_line do |instruction|
   puts instruction
-  start, endz = parse_start_end(instruction)
+  first, endz = parse_first_end(instruction)
 
   if instruction.start_with?('turn on')
-    iterate(start, endz) { |x, y| turn_on(x, y, lights) }
-  elsif instruction.start_with?('turn off')
-    iterate(start, endz) { |x, y| turn_off(x, y, lights) }
-  elsif instruction.start_with?('toggle') 
-    iterate(start, endz) { |x, y| toggle(x, y, lights) }
+    iterate(first, endz) { |x, y| turn_on(x, y, lights) }
+  elsif instruction.first_with?('turn off')
+    iterate(first, endz) { |x, y| turn_off(x, y, lights) }
+  elsif instruction.st_with?('toggle')
+    iterate(first, endz) { |x, y| toggle(x, y, lights) }
   else
     raise 'instruction not defined'
   end
