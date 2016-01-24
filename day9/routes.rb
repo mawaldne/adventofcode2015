b43b228868513b98efdc42128a123ad24e81ee2f
Mike Waldner <mike.waldner@redfall.com>
2016-01-24 15:41:06 -0700

    Some improvements with inject.

--- day9/routes.rb
+++ day9/routes.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby -w
 require 'set'
- 
+
 distances = {}
 routes = {}
 cities = Set.new
@@ -10,18 +10,18 @@ ARGF.each_line do |route|
   next unless route =~ ROUTE_FORMAT
   distances[[$1, $2]] = Integer($3)
   distances[[$2, $1]] = Integer($3)
-  
+
   cities << $1 << $2
 end
 
-cities.to_a.permutation(cities.size).each do |route| 
-  next if route.uniq.size < cities.size
-  distance = 0
-  route.each_cons(2).each do |pair|
-    distance += distances[pair] 
-  end 
-  routes[route] = distance
-end 
+cities.to_a.permutation(cities.size).each do |route|
+
+  routes[route] = route
+    .each_cons(2)
+    .map { |pair| distances[pair] }
+    .inject(:+)
+
+end
 
 p "#{routes.values.min}"
 p "#{routes.values.max}"
