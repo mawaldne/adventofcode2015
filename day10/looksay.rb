90a733ab8eafa1d7d785437c8bf8657bbbbd7064
Mike Waldner <mike.waldner@redfall.com>
2016-01-24 22:00:11 -0700

    A very very slow day 10...

---
+++ day10/looksay.rb
@@ -0,0 +1,16 @@
+# Ruby 2.2.3. Using slice_when method
+
+def looksay(s)
+  s.split('')
+  .slice_when {|i,j| i != j }
+  .map { |a| "#{a.size}#{a[0]}" }
+  .join('')
+end
+
+
+s = '1113122113'
+1.upto(50).each do |i|
+  s = looksay(s)
+end
+
+puts s.size
