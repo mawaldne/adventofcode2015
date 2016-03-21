1320f03f3084e025d995a9bcf243783938315adf
Mike Waldner <mike.waldner@redfall.com>
2016-03-20 21:23:49 -0600

    Cleanup from other solutions

--- day13/seats.rb
+++ day13/seats.rb
@@ -11,7 +11,7 @@ class Table
   end 
 
   def add(happiness)
-    fail "Bad happiness str: #{happiness} " unless happiness =~ /\A(\w+) would (\w+) (\d+) happiness units by sitting next to (\w+)\.\Z/
+    fail "Bad happiness str: #{happiness} " unless happiness =~ /\A(\w+)\b.+\b(\w+)\s(\d+)\b.+\b(\w+)\.\Z/
     name, value, next_to  = $1, $3.to_i, $4
 
     value = $2 == 'gain' ? value * 1 : value * -1
@@ -24,19 +24,12 @@ class Table
     max_arrangement = []
     current_max = 0
 
-    @names.to_a.permutation(@names.count).each do |arrangement|
+    @names.to_a.permutation.each do |arrangement|
       max = 0
-      arrangement.each_with_index do |value, index|
-         if index == 0
-           max += @happiness[[value, arrangement[index + 1]]]
-           max += @happiness[[value, arrangement[arrangement.length - 1]]]
-         elsif index == arrangement.length - 1
-           max += @happiness[[value, arrangement[index - 1]]]
-           max += @happiness[[value, arrangement[0]]]
-         else
-           max += @happiness[[value, arrangement[index + 1]]]
-           max += @happiness[[value, arrangement[index - 1]]]
-         end
+      cirle = [arrangement.last] + arrangement + [arrangement.first]
+      cirle.each_cons(3) do |left, middle, right|
+        max += @happiness[[middle, left]]
+        max += @happiness[[middle, right]]
       end
 
       if max > current_max
@@ -49,7 +42,6 @@ class Table
   end
 end 
 
-
 table = Table.new
 ARGF.each_line do |happiness|
   table.add(happiness)
@@ -57,6 +49,3 @@ end
 
 p table.max_happiness
  
-
-
-
